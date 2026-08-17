a0e7ca96ba67c92afdfd3ce61247947ea8a43b0a
Watson Hartsoe <watson.hartsoe@gmail.com>
2026-08-17 10:29:14 -0400

    Load III architecture v3 after fail-closed reference layer

--- terrarium-iii-store.js
+++ terrarium-iii-store.js
@@ -7,4 +7,5 @@ import('./terrarium-iii-worldtext.js')
   .then(() => import('./terrarium-iii-house-bind.js'))
   .then(() => import('./terrarium-iii-reference.js'))
   .then(() => import('./terrarium-iii-reference-guard.js'))
+  .then(() => import('./terrarium-iii-architecture-v3.js'))
   .catch(err => console.warn('[III COMPILER] optional sidecar unavailable', err));
